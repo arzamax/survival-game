934403fe3b67490fc7dd522ca364cc7b1854ef32
Maksim Arzamastsev <arzamax@gmail.com>
2021-06-29 21:49:01 +0400

    feat: Added animation

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,10 +1,7 @@
 import Phaser from 'phaser';
-import playerImage from '../assets/player_sprite.png';
-import playerAtlas from '../assets/player_atlas.json';
-import playerAnimation from '../assets/player_anim.json';
 
 export class MainScene extends Phaser.Scene {
-  private player?: Phaser.Physics.Matter.Sprite;
+  private monster?: Phaser.Physics.Matter.Sprite;
   private inputKeys?: Record<string, Phaser.Input.Keyboard.Key>;
 
   constructor() {
@@ -12,13 +9,13 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.atlas('player', playerImage, playerAtlas);
-    this.load.animation('player_anim', (playerAnimation as unknown) as string);
+    this.load.atlas('monster', '/assets/monster_sprite.png', '/assets/monster_atlas.json');
+    this.load.animation('monster_anim', '/assets/monster_anim.json');
   }
 
   create() {
-    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world, 200 , 200, 'player', 'big_demon_idle_anim_f0');
-    this.add.existing(this.player);
+    this.monster = new Phaser.Physics.Matter.Sprite(this.matter.world, 200 , 200, 'monster', 'big_demon_idle_anim_f0');
+    this.add.existing(this.monster);
     this.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -28,22 +25,23 @@ export class MainScene extends Phaser.Scene {
   }
 
   update() {
-    const playerVelocity = new Phaser.Math.Vector2();
+    this.monster?.anims.play('monster_walk', true);
+    const monsterVelocity = new Phaser.Math.Vector2();
 
     if (this.inputKeys?.left.isDown) {
-      playerVelocity.x = -1;
+      monsterVelocity.x = -1;
     } else if (this.inputKeys?.right.isDown) {
-      playerVelocity.x = 1;
+      monsterVelocity.x = 1;
     }
 
     if (this.inputKeys?.up.isDown) {
-      playerVelocity.y = -1;
+      monsterVelocity.y = -1;
     } else if (this.inputKeys?.down.isDown) {
-      playerVelocity.y = 1;
+      monsterVelocity.y = 1;
     }
 
-    playerVelocity.normalize();
-    playerVelocity.scale(2.5);
-    this.player?.setVelocity(playerVelocity.x, playerVelocity.y);
+    monsterVelocity.normalize();
+    monsterVelocity.scale(2.5);
+    this.monster?.setVelocity(monsterVelocity.x, monsterVelocity.y);
   }
 }
